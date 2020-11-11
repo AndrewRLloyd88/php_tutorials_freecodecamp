f4ef0561f41f2a3b2a12541a4e8f312a148371ea
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-11-11 12:32:19 -0800

    check one more line onto variables and working ok

--- Hello World/hello.php
+++ Hello World/hello.php
@@ -7,11 +7,13 @@
 <?php
 $characterAge = 86;
 $characterName = "Michael";
+$characterState = "North Alabama";
   echo "Hello, World!";
   echo "<p>There once was a man named $characterName<p>";
   echo "<p>He was $characterAge years old<p>";
   echo "<p>He really liked the name $characterName<p>";
   echo "<p>He hated being $characterAge years old<p>";
+  echo "<p>He lived in the state of $characterState <p>";
 ?>
 </body>
 </html>
