c0d66ea363ba68f0781972aeef28cd6dd3ab83ff
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-11-11 12:30:29 -0800

    add file to directory

---
+++ Hello World/hello.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello, World! Page</title>
+</head>
+<body>
+<?php
+$characterAge = 86;
+$characterName = "Michael";
+  echo "Hello, World!";
+  echo "<p>There once was a man named $characterName<p>";
+  echo "<p>He was $characterAge years old<p>";
+  echo "<p>He really liked the name $characterName<p>";
+  echo "<p>He hated being $characterAge years old<p>";
+?>
+</body>
+</html>
